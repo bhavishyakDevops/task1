f69dd8bde66850082f673255c3d186ebdc2f784a
Bhavishya <137475435+bhavishyakDevops@users.noreply.github.com>
2023-07-18 11:59:31 +0530

    Update amazon.c

--- amazon.c
+++ amazon.c
@@ -1,14 +1,11 @@
-s.no:   customer name:         customer ph num:         email id:             order id:           payment status:          product status:
-
+S.No:   Customer Name:         Customer Ph Num:         Email ID:             Order ID:           Payment Status:          Product Status:
 1.      Sreekanth                xxxxxxxxxx          sree16@gmail.com           67462                online                   delivered
-2.      Santhosh             	 xxxxxxxxxx          santhu8@gmail.com          35468                cash on del              not delivered
-3.	Sravan   	 	 xxxxxxxxxx  	     srav23@gmail.com		78453		     online                   not delivered
-4.	Naveen			 xxxxxxxxxx	     navi9@gmail.com	        98354                online		      delivered
-5.	Jay			 xxxxxxxxxx	     jay10@gmail.com		74658		     cash on del	      delivered
-6. 	Ashok			 xxxxxxxxxx	     ash3@gmail.com		24356		     online		      delivered
-7.	Sanjay			 xxxxxxxxxx	     sanju08@gmail.com		94657		     online		      delivered
-8.	Jashu			 xxxxxxxxxx   	     jash7@gmail.com	        46537                cash on del              not delivered
-9.	Bhavishya		 xxxxxxxxxx	     bhavi08@gmail.com 		58746 		     online                   not delivered
-10.	Naishithya		 xxxxxxxxxx          naishi9@gmal.com		43567		     online		      delivered
-
-
+2.      Santhosh             	   xxxxxxxxxx          santhu8@gmail.com          35468                cash on del              not delivered
+3.	    Sravan   	 	             xxxxxxxxxx  	       srav23@gmail.com		        78453		             online                   not delivered
+4.	    Naveen			             xxxxxxxxxx	         navi9@gmail.com	          98354                online		                delivered
+5.	    Jay			                 xxxxxxxxxx	         jay10@gmail.com		        74658		             cash on del	            delivered
+6. 	    Ashok			               xxxxxxxxxx	         ash3@gmail.com		          24356		             online		                delivered
+7.	    Sanjay			             xxxxxxxxxx	         sanju08@gmail.com		      94657		             online		                delivered
+8.	   Jashu			               xxxxxxxxxx   	     jash7@gmail.com	          46537                cash on del              not delivered
+9.	  Bhavishya		               xxxxxxxxxx	         bhavi08@gmail.com 		      58746 		           online                   not delivered
+10.	  Naishithya		             xxxxxxxxxx          naishi9@gmal.com		        43567		             online		                delivered
