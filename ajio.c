f2d5f74cc38bba9c53de8c2ed46a65554fe7f724
root <root@linuxserver.com>
2023-07-18 11:36:58 +0530

    sales

---
+++ ajio.c
@@ -0,0 +1,14 @@
+.no:   customer name:         customer ph num:         email id:             order id:           payment status:          product status:
+
+1.      Shyam                    xxxxxxxxxx          shyam16@gmail.com          67462                online                   delivered
+2.      Shiva                    xxxxxxxxxx          shiv@gmail.com             35468                cash on del              not delivered
+3.      Sreekar                  xxxxxxxxxx          sree23@gmail.com           78453                online                   not delivered
+4.      Varun                    xxxxxxxxxx          varun9@gmail.com           98354                online                   delivered
+5.      Teja                     xxxxxxxxxx          tej10@gmail.com            74658                cash on del              delivered
+6.      Chakri                   xxxxxxxxxx          chakri3@gmail.com          24356                online                   delivered
+7.      Hari                     xxxxxxxxxx          hari08@gmail.com           94657                online                   delivered
+8.      Tirunath                 xxxxxxxxxx          thiru7@gmail.com           46537                cash on del              not delivered
+9.      Sashank                  xxxxxxxxxx          sash08@gmail.com           58746                online                   not delivered
+10.     Sai                      xxxxxxxxxx          sai9@gmal.com              43567                online                   delivered
+
+
---
+++ flipkart.c
@@ -0,0 +1,14 @@
+S.no:   customer name:         customer ph num:         email id:             order id:           payment status:          product status:
+
+1.      Himaja                   xxxxxxxxxx          hima16@gmail.com           67462                online                   delivered
+2.      Moksha                   xxxxxxxxxx          moki@gmail.com             35468                cash on del              not delivered
+3.      Sharanya                 xxxxxxxxxx          sharu23@gmail.com          78453                online                   not delivered
+4.      Sangeetha                xxxxxxxxxx          santha9@gmail.com          98354                online                   delivered
+5.      Samyuktha                xxxxxxxxxx          samyu10@gmail.com          74658                cash on del              delivered
+6.      Mounika                  xxxxxxxxxx          mouni3@gmail.com           24356                online                   delivered
+7.      Vamsi                    xxxxxxxxxx          vmsi08@gmail.com           94657                online                   delivered
+8.      Jahnavi                  xxxxxxxxxx          janu7@gmail.com            46537                cash on del              not delivered
+9.      Deepak                   xxxxxxxxxx          deepak08@gmail.com         58746                online                   not delivered
+10.     Manu                     xxxxxxxxxx          manu9@gmal.com             43567                online                   delivered
+
+
